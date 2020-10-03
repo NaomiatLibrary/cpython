bb12f5cc1bde2728412ccd14a9fddcace130f333
NaomiatLibrary <ray.oshikawa@gmail.com>
2020-10-03 20:43:14 +0900

    tokens and grammers

--- Include/Python-ast.h
+++ Include/Python-ast.h
@@ -304,14 +304,15 @@ struct _stmt {
     int end_col_offset;
 };
 
-enum _expr_kind {BoolOp_kind=1, NamedExpr_kind=2, BinOp_kind=3, UnaryOp_kind=4,
-                  Lambda_kind=5, IfExp_kind=6, Dict_kind=7, Set_kind=8,
-                  ListComp_kind=9, SetComp_kind=10, DictComp_kind=11,
-                  GeneratorExp_kind=12, Await_kind=13, Yield_kind=14,
-                  YieldFrom_kind=15, Compare_kind=16, Call_kind=17,
-                  FormattedValue_kind=18, JoinedStr_kind=19, Constant_kind=20,
-                  Attribute_kind=21, Subscript_kind=22, Starred_kind=23,
-                  Name_kind=24, List_kind=25, Tuple_kind=26, Slice_kind=27};
+enum _expr_kind {BoolOp_kind=1, NamedExpr_kind=2, BinOp_kind=3, IncOp_kind=4,
+                  UnaryOp_kind=5, Lambda_kind=6, IfExp_kind=7, Dict_kind=8,
+                  Set_kind=9, ListComp_kind=10, SetComp_kind=11,
+                  DictComp_kind=12, GeneratorExp_kind=13, Await_kind=14,
+                  Yield_kind=15, YieldFrom_kind=16, Compare_kind=17,
+                  Call_kind=18, FormattedValue_kind=19, JoinedStr_kind=20,
+                  Constant_kind=21, Attribute_kind=22, Subscript_kind=23,
+                  Starred_kind=24, Name_kind=25, List_kind=26, Tuple_kind=27,
+                  Slice_kind=28};
 struct _expr {
     enum _expr_kind kind;
     union {
@@ -330,6 +331,10 @@ struct _expr {
             operator_ty op;
             expr_ty right;
         } BinOp;
+
+        struct {
+            expr_ty right;
+        } IncOp;
 
         struct {
             unaryop_ty op;
@@ -655,6 +660,9 @@ expr_ty _Py_NamedExpr(expr_ty target, expr_ty value, int lineno, int
 expr_ty _Py_BinOp(expr_ty left, operator_ty op, expr_ty right, int lineno, int
                   col_offset, int end_lineno, int end_col_offset, PyArena
                   *arena);
+#define IncOp(a0, a1, a2, a3, a4, a5) _Py_IncOp(a0, a1, a2, a3, a4, a5)
+expr_ty _Py_IncOp(expr_ty right, int lineno, int col_offset, int end_lineno,
+                  int end_col_offset, PyArena *arena);
 #define UnaryOp(a0, a1, a2, a3, a4, a5, a6) _Py_UnaryOp(a0, a1, a2, a3, a4, a5, a6)
 expr_ty _Py_UnaryOp(unaryop_ty op, expr_ty operand, int lineno, int col_offset,
                     int end_lineno, int end_col_offset, PyArena *arena);
